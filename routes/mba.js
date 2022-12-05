249b5551d55e7127083a50d0b9b78a9b9ec8dc8a
Mohammed Afkar <afkarabi34@gmail.com>
2022-12-05 12:39:28 +0530

    updates

---
+++ routes/mba.js
@@ -0,0 +1,104 @@
+
+//mba datas
+
+//1
+let mba=[{title:"WISDOM-MAN-CORPORATE-MANAGER",
+para:"",
+grules:"General Rules:",
+rule1:"Each round will be 10-15 minutes each.",
+rule2:"The decision of the authorities will be final and irrefutable.",
+rule3:"",
+rule4:"",
+room:"Room No. 210",fee1:"Rs. 50/- per head",
+fee2:"",coordinator1:" Mohammed Nihal",
+coordinator1no:"9526330896",
+coordinator2:" Sreekala R",
+coordinator2no:"8606314431"},
+
+
+//2
+{title:"BOOM-BANG",
+para:"",
+grules:"General Rules:",
+rule1:"",
+rule2:"",
+rule3:"",
+rule4:"",
+room:"",fee1:"",
+fee2:"",coordinator1:"",
+coordinator1no:"",
+coordinator2:"",
+coordinator2no:""},
+
+//2
+{title:"FOOTBALL-FEVER",
+para:"",
+grules:"General Rules:",
+rule1:"",
+rule2:"",
+rule3:"",
+rule4:"",
+room:"",fee1:"",
+fee2:"",coordinator1:"",
+coordinator1no:"",
+coordinator2:"",
+coordinator2no:""},
+
+//2
+{title:"PARALLEL-WORLD-AR-VR",
+para:"",
+grules:"General Rules:",
+rule1:"",
+rule2:"",
+rule3:"",
+rule4:"",
+room:"",fee1:"",
+fee2:"",coordinator1:"",
+coordinator1no:"",
+coordinator2:"",
+coordinator2no:""},
+
+//2
+{title:"LETS-NACHO",
+para:"",
+grules:"General Rules:",
+rule1:"",
+rule2:"",
+rule3:"",
+rule4:"",
+room:"",fee1:"",
+fee2:"",coordinator1:"",
+coordinator1no:"",
+coordinator2:"",
+coordinator2no:""},
+
+//2
+{title:"MIND-TWISTER",
+para:"",
+grules:"General Rules:",
+rule1:"",
+rule2:"",
+rule3:"",
+rule4:"",
+room:"",fee1:"",
+fee2:"",coordinator1:"",
+coordinator1no:"",
+coordinator2:"",
+coordinator2no:""},
+
+//2
+{title:"DRAGON-SHOE",
+para:"",
+grules:"General Rules:",
+rule1:"",
+rule2:"",
+rule3:"",
+rule4:"",
+room:"",fee1:"",
+fee2:"",coordinator1:"",
+coordinator1no:"",
+coordinator2:"",
+coordinator2no:""}
+]
+
+
